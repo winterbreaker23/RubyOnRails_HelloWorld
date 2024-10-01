1121452ac7c482c4f28ea1493ca4191c506c4321
winterbreaker <133730216+winterbreaker23@users.noreply.github.com>
2024-10-01 14:54:23 +0000

    add new articles page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  resources :articles
+
   root "welcome#index"
 
   # Defines the root path route ("/")
